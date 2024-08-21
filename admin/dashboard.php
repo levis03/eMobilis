612a853b5b58e779d76bea01964d131941b31fb2
Levis Maina <levismaina03@gmail.com>
2024-08-21 14:19:23 +0300

    "Updated dashboard.php, styles.css, and slicknav.min.css files with various changes to HTML structure, CSS styles, and removed chart-related code."

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -115,11 +115,11 @@
                 <!-- sales report area start -->
                 <div class="sales-report-area mt-5 mb-5">
                     <div class="row">
-                        <div class="col-md-4">
+                        <!-- <div class="col-md-4">
                             <div class="single-report mb-xs-30">
                                 <div class="s-report-inner pr--20 pt--30 mb-3">
-                                    <!-- <div class="icon"><i class="fa fa-briefcase"></i></div> -->
-                                    <div class="s-report-title d-flex justify-content-between">
+                                     <div class="icon"><i class="fa fa-briefcase"></i></div> -->
+                                  <!--  <div class="s-report-title d-flex justify-content-between">
                                         <h4 class="header-title mb-0">Available Leave Types</h4>
                                         
                                     </div>
@@ -128,9 +128,9 @@
                                         <span>Leave Types</span>
                                     </div>
                                 </div>
-                                <!-- <canvas id="coin_sales1" height="100"></canvas> -->
-                            </div>
-                        </div>
+                                 <canvas id="coin_sales1" height="100"></canvas> -->
+                          <!--  </div>
+                        </div> -->
                         <div class="col-md-4">
                             <div class="single-report mb-xs-30">
                                 <div class="s-report-inner pr--20 pt--30 mb-3">
@@ -149,27 +149,6 @@
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="single-report">
-                                <div class="s-report-inner pr--20 pt--30 mb-3">
-                                    <!-- <div class="icon"><i class="fa fa-th-large"></i></div> -->
-                                    <div class="s-report-title d-flex justify-content-between">
-                                        <h4 class="header-title mb-0">Available Departments</h4>
-                                        
-                                    </div>
-                                    <div class="d-flex justify-content-between pb-2">
-                                        <h1><?php include 'counters/dept-counter.php'?></h1>
-                                        <span>Employee Departments</span>
-                                    </div>
-                                </div>
-                                <!-- <canvas id="coin_sales3" height="100"></canvas> -->
-                            </div>
-                        </div>
-                    </div>
-
-                    <br>
-
-                    <div class="row">
-                        <div class="col-md-4">
                             <div class="single-report mb-xs-30">
                                 <div class="s-report-inner pr--20 pt--30 mb-3">
                                     <!-- <div class="icon"><i class="fa fa-spinner"></i></div> -->
@@ -185,6 +164,27 @@
                                 <!-- <canvas id="coin_sales1" height="100"></canvas> -->
                             </div>
                         </div>
+                        <!-- <div class="col-md-4">
+                            <div class="single-report">
+                                <div class="s-report-inner pr--20 pt--30 mb-3">
+                                     <div class="icon"><i class="fa fa-th-large"></i></div> -->
+                                   <!-- <div class="s-report-title d-flex justify-content-between">
+                                        <h4 class="header-title mb-0">Available Departments</h4>
+                                        
+                                    </div>
+                                    <div class="d-flex justify-content-between pb-2">
+                                        <h1><?php include 'counters/dept-counter.php'?></h1>
+                                        <span>Employee Departments</span>
+                                    </div>
+                                </div>
+                             <canvas id="coin_sales3" height="100"></canvas>
+                            </div>
+                        </div> -->
+                    </div>
+
+                    <br>
+
+                    <div class="row">
                         <div class="col-md-4">
                             <div class="single-report mb-xs-30">
                                 <div class="s-report-inner pr--20 pt--30 mb-3">
@@ -237,9 +237,9 @@
                                             </li>
                                         </ul>
                                     </div>
-                                    <select class="custome-select border-0 pr-3">
-                                        <option selected>Last 24 Hours</option>
-                                        
+                                    <!-- select element to trigger a JS function that sends the selected filter option to the server -->
+                                    <select class="custome-select border-0 pr-3" id="filterPeriod" onchange="filterData()">
+                                        <option value="24">Last 24 Hours</option>
                                     </select>
                                 </div>
                                         <!-- <h4 class="header-title"></h4> -->
@@ -328,24 +328,11 @@
     <script src="../assets/js/jquery.slimscroll.min.js"></script>
     <script src="../assets/js/jquery.slicknav.min.js"></script>
 
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-    <script>
-    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
-    <!-- all line chart activation -->
-    <script src="assets/js/line-chart.js"></script>
-    <!-- all pie chart -->
-    <script src="assets/js/pie-chart.js"></script>
     
     <!-- others plugins -->
     <script src="../assets/js/plugins.js"></script>
     <script src="../assets/js/scripts.js"></script>
+    
 </body>
 
 </html>
